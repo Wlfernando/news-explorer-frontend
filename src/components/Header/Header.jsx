e3233ed904157494bacc6e980a618ef46b5801be
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-02-20 19:20:48 -0600

    style: add background

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,11 @@ import Navigation from '../Navigation/Navigation.jsx';
 
 export default function Header() {
 
-  return <header className="header">
-    <h1 className="header__title">NewsExplorer</h1>
-    <Navigation />
+  return <header className="header header_history_main">
+    <div className="header__nav">
+      <h1 className="header__title">NewsExplorer</h1>
+      <Navigation />
+    </div>
+    
   </header>
 }
