00d5b7d6f49a78ba93d771d71185b89a429291a5
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-02-24 16:29:18 -0600

    feat: add about

---
+++ src/components/About/About.jsx
@@ -0,0 +1,17 @@
+import './About.css'
+import author from '../../images/author-photo.jpg'
+
+export default function About() {
+  return <section className='about'>
+    <img src={author} alt="foto del autor" className="about__image" />
+    <div className="about__brief">
+      <h2 className="about__title">Acerca del autor</h2>
+      <p className="about__text">¡Muy buenas! Mi nombre es Fernando y soy de México.</p>
+      <p className="about__text">Me forme en Tripleten como fullStack MERN, ahí conocí a muy buenos integrantes del equipo y algunos grandes compañeros.
+      Fue muy interesante asumir el reto de principio a fin al desarrollar solo las paginas web para el aprendizaje.
+      </p>
+      <p className="about__text">Encontre muy entretenido resolver ejercicios de codewars para familiarizarme con el lenguaje de javascript.</p>
+      <p className="about__text">Además como prácticas, desarrollo en No Country en un equipo para adquirir habilidades blandas y poder sacar en un sprint un MVP.</p>
+    </div>
+  </section>
+}
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,4 @@
+import About from '../About/About.jsx'
 import Header from '../Header/Header.jsx'
 import './App.css'
 
@@ -5,6 +6,7 @@ export default function App() {
   return (
     <div className='page'>
       <Header />
+      <About />
     </div>
   )
 }
