41fc21e6843e90ecfcb8c6f02dbc2e1f051a3b4f
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-02-26 13:51:38 -0600

    Feat: add SearchResults

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,14 @@
 import About from '../About/About.jsx'
 import Footer from '../Footer/Footer.jsx'
 import Header from '../Header/Header.jsx'
-import NewsCardList from '../NewsCardList/NewsCardList.jsx'
-import SavedNews from '../SavedNews/SavedNews.jsx'
-import SearchedNews from '../ShearchedNews/SearchedNews.jsx'
+import SearchRsults from '../SearchResults/SearchResults.jsx'
 import './App.css'
 
 export default function App() {
   return (
     <div className='page'>
       <Header />
-      <NewsCardList>
-        {Array(5).fill(<SearchedNews />)}
-      </NewsCardList>
+      <SearchRsults />
       <About />
       <Footer />
     </div>
---
+++ src/components/SearchResults/SearchResults.jsx
@@ -0,0 +1,13 @@
+import NewsCardList from '../NewsCardList/NewsCardList.jsx';
+import SearchedNews from '../ShearchedNews/SearchedNews.jsx';
+import './SearchResults.css';
+
+export default function SearchRsults() {
+  return <section className='search-results'>
+    <h2 className="search-results__title">Resultados de la Búsqueda</h2>
+    <NewsCardList>
+      {Array(5).fill(<SearchedNews />)}
+    </NewsCardList>
+    <button type="button" className='search-results__button'>Ver más</button>
+  </section>
+}
