1a305e39558b9e0c1879993d6d2896b5929aede4
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-02-28 11:40:17 -0600

    feat: add newsInfo

--- src/components/Main/Home/Home.jsx
+++ src/components/Main/Home/Home.jsx
@@ -4,12 +4,14 @@ import SignInModal from "../../Modals/SignInModal/SignInModal.jsx";
 import SignUpModal from "../../Modals/SignUpModal/SignUpModal.jsx";
 import InfoToolTip from "../../Modals/InfoToolTip/InfoToolTip.jsx";
 import HomeHeader from "../../Header/HomeHeader/HomeHeader.jsx";
+import NewsInfo from "../../NewsInfo/NewsInfo.jsx";
 
 export default function Home({
   modals: [{signIn, signUp, toolTip}, openModal]
 }) {
   return <main>
     <HomeHeader />
+    <NewsInfo />
     <SearchRsults />
     <About />
     <SignInModal isOpen={signIn} />
