27a69f850dc67defd976f111606c17de26e77778
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-03-05 11:05:34 -0600

    feat: enable validation forms

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,32 @@
+import { useRef, useState } from 'react';
 import './Form.css';
 
-export default function Form({name, className, onSubmit, children, btnTxt, modBtn}) {
+export default function Form({name, className, onSubmit, children, btnTxt, modBtn, onChange}) {
+  const hasInputs = useRef(Boolean(children))
+  const [disabled, setDisabled] = useState(hasInputs.current);
+
   function handleSubmit(e) {
     e.preventDefault()
-    onSubmit(e)
+    onSubmit(e, ()=> setDisabled(hasInputs.current))
   }
 
-  return <form className={className} name={name} noValidate onSubmit={handleSubmit}>
+  function handleValidation(e) {
+    const
+      form = e.currentTarget.elements,
+      isValid = Array(...form)
+        .every(input => input.validity.valid)
+
+    setDisabled(!isValid)
+
+    if(onChange && typeof onChange === 'function') onChange(e)
+  }
+
+  return <form className={className} name={name} noValidate onSubmit={handleSubmit} onChange={handleValidation}>
     {children}
-    <button className={`form__button${modBtn ? ' form__button_type_' + modBtn : ''}`} type="submit" >{btnTxt}</button>
+    <button
+      className={`form__button${modBtn ? ' form__button_type_' + modBtn : ''}`}
+      disabled={disabled}
+      type="submit" >{btnTxt}
+    </button>
   </form>
 }
