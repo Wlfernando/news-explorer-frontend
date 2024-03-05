8f39d1327eedbc167549e3bf94e8ddcfc5037ef3
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-03-04 23:49:28 -0600

    feat: add context for user

--- src/hooks/useGlobalContext.js
+++ src/hooks/useGlobalContext.js
@@ -2,13 +2,18 @@ import { createContext, useContext, useState } from "react";
 import useModal from "./useModal";
 
 const ModalContext = createContext({})
+const UserContext = createContext({})
 
 export const GlobalContextProvider = ({children}) => {
   const [{signIn, signUp, infoTooltip}, openPopup, closeAllPopups] = useModal('signIn', 'signUp', 'infoToolTip');
+  const [user] = useState({});
 
   return <ModalContext.Provider value={{signIn, signUp, infoTooltip, openPopup, closeAllPopups}}>
-    {children}
+    <UserContext.Provider value={user}>
+      {children}
+    </UserContext.Provider>
   </ModalContext.Provider>
 }
 
 export const useModalContext = () => useContext(ModalContext)
+export const useUserContext = () => useContext(UserContext)
