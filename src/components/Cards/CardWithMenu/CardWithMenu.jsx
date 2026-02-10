0a90e4e01038b01a95d1e30e994ed9826f774c52
Fernando Alcázar Malfavón <4maferal@gmail.com>
2026-02-10 03:22:52 -0600

    Refactor: prevent re-call Intl for date on NewsCard

--- src/components/Cards/CardWithMenu/CardWithMenu.jsx
+++ src/components/Cards/CardWithMenu/CardWithMenu.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './CardWithMenu.css';
 
 export default function NewsCard({
@@ -9,19 +10,19 @@ export default function NewsCard({
   description,
   url,
 }) {
-  const formatDate = new Intl.DateTimeFormat('es-MX', {
+  const [formatedDate] = useState(() => new Intl.DateTimeFormat('es-MX', {
     year: 'numeric',
     month: 'long',
     day: 'numeric'
-  }).format(new Date(publishedAt))
+  }).format(new Date(publishedAt)))
 
   return (
   <li className='news-card'>
     <figure className='news-card__figure'>
-      <img src={urlToImage} alt="" className="news-card__image" />
+      <img src={urlToImage} alt="noticia" className="news-card__image" />
       {children}
     </figure>
-    <p className="news-card__date">{formatDate}</p>
+    <p className="news-card__date">{formatedDate}</p>
     <h3 className="news-card__title">{title}</h3>
     <p className="news-card__text">{description}</p>
     <a href={url} className="news-card__cite">{name}</a>
